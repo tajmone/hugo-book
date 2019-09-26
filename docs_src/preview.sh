d754d8262b40eb7711818ca4c22ac3a92be06213
tajmone <tajmone@libero.it>
2019-09-26 06:02:26 +0200

    Preview Script: TOC Levels 1

--- docs_src/preview.sh
+++ docs_src/preview.sh
@@ -5,11 +5,16 @@ source=hugo_book.asciidoc
 echo -e "\n\e[94m###########################"
 echo -e "#\e[93m Hugo Book: HTML Preview \e[94m#"
 echo -e "###########################\e[0m"
-echo -e "Building a quick HTML preview: single doc, default template, no syntax highlighting."
+echo -e "Building a quick HTML preview:"
+echo -e "\e[91m *\e[0m TOC levels: 1."
+echo -e "\e[91m *\e[0m Single document."
+echo -e "\e[91m *\e[0m Default template."
+echo -e "\e[91m *\e[0m No syntax highlighting."
 
 asciidoctor \
   --verbose \
   --safe-mode unsafe \
+  -a toclevels=1 \
   -a reproducible \
   $source
 
